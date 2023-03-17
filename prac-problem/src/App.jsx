8286a46dbd9c18fdd3f82d33be61a2428f34661a
martinyis <martinyis11@gmail.com>
2023-03-16 20:28:15 -0400

    first commit

---
+++ prac-problem/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import "./assets/styles/global.css";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
